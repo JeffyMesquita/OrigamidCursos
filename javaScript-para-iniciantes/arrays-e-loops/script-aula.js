e408479facfaef1a8c80c5fb0395c383c4560d9e
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2021-01-19 01:18:17 -0300

    Arrays e Loops
    Finalização de Arrays e Loops

--- javaScript-para-iniciantes/arrays-e-loops/script-aula.js
+++ javaScript-para-iniciantes/arrays-e-loops/script-aula.js
@@ -1,5 +1,38 @@
-var nome = 'Jeferson';
+/*var videoGames = ['Switch', 'PS4', 'XBOX'];
 
-var nomeMinusculo = nome.toLocaleLowerCase();
+var ultimoItem = videoGames.pop(); */
 
-var btn = document.querySelector('.btn');
+for (var numero = 0; numero < 10; numero++) {
+      console.log(numero);
+};
+
+var i = 0;
+while (i < 10) {
+      console.log(i);
+      i++;
+}// Retorna de 0 a 9 no console
+
+var videoGames = [ 'Switch', 'PS4', 'XBox', '3DS'];
+
+for  (var item = 0; item < videoGames.length; item++) {
+      console.log(videoGames[item]);
+      if (videoGames[item] === 'PS4') {
+            break;
+      }
+}
+
+var frutas = ['Banana', 'Pera', 'Maçã', 'Abacaxi', 'Uva'];
+
+frutas.forEach(function (fruta, index)  {
+      frutas.pop();
+      console.log(fruta, index, frutas);
+});
+
+var numero = 0;
+var maximo = 50;
+for(;numero < maximo;) {
+      console.log(numero);   
+      numero++;
+}
+
+
--- javaScript-para-iniciantes/arrays-e-loops/script.js
+++ javaScript-para-iniciantes/arrays-e-loops/script.js
@@ -1,19 +1,25 @@
-// nomeie 3 propriedades ou métodos de strings
-var nome = 'Jeferson';
-fixed 
-length
-slice 
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
 
-// nomeie 5 propriedades ou métodos de elementos do DOM
-var btn = document.querySelector('.btn');
-// addEventListenner
-// appendChild
-//  id
-// innerHTML
-// outerHtml
+var brasilCampeao = ['1950', '1962', '1970', '1994', '2002', ''];
 
-console.log(btn);
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
 
-// busque na web um objeto (método) capaz de interagir com o clipboard, 
-// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+for (var i = 0; i < brasilCampeao.length; i++) {
+      console.log(`O Brasil ganhou a copa de ${brasilCampeao[i]}`);
+}
 
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+for (var fruta = 0; fruta < frutas.lengght; fruta++){
+      console.log(frutas[fruta]);
+      if(frutas[fruta] === 'Pera') {
+            break;
+      }
+}
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+var ultimaFruta = frutas[frutas.length - 1];
