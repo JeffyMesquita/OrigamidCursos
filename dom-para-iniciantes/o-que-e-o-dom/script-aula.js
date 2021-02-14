cd7a3ccde45005c12424d93ae9951ffaf8c1a373
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2021-02-14 13:30:23 -0300

    O que é Dom
    Parte 1 e
    Parte 2

---
+++ dom-para-iniciantes/o-que-e-o-dom/script-aula.js
@@ -0,0 +1,18 @@
+// window.alert('isso mesmo!');
+
+const href = window.location.href;
+
+console.log(href);
+
+if(href === 'http://127,0,0,1:5500/o-que-e-o-dom/') {
+   console.log('É igual')
+}
+
+const h1Selecionado = document.querySelector('h1');
+const h1Classes = h1Selecionado.classList;
+
+function callbackh1() {
+   console.log('Clicou em', h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', callbackh1)
