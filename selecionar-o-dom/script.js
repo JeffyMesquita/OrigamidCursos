e2bf69ece9828740326fb7ff89d5ed3b3a1995b9
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2021-02-15 15:28:22 -0300

    Exemplo de Projeto
    Parte 3
    Seleção de Elementos 1

---
+++ selecionar-o-dom/script.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById('animais');
+console.log(animais.innerText);
+
+const gridSection = document.getElementsByClassName('grid-section');
+
+console.log(gridSection[1]);
+
+const primeilaLi = document.querySelector('li');
+console.log(primeilaLi)
+
+const primeilaUl = document.querySelector('ul');
+console.log(primeilaUl)
+
+const linkInterno = document.querySelector('[href^="#"]');console.log(linkInterno);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[2]);
