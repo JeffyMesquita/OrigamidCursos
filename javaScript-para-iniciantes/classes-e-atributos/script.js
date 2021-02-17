e7716cde5e7a7081e02a08f25a1788c4fe30e833
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2021-02-17 00:03:41 -0300

    Classes e Atributos
    Parte 2

--- javaScript-para-iniciantes/classes-e-atributos/script.js
+++ javaScript-para-iniciantes/classes-e-atributos/script.js
@@ -1,23 +1,28 @@
-const menu = document.querySelector('.menu');
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a');
 
-menu.classList.add('ativo', 'teste');
-menu.classList.remove('azul');
+itensMenu.forEach((item) => {
+   item.classList.add('ativo');
+});
 
-if(menu.classList.contains('azul')) {
-   menu.classList.add('possui-azul');
-} else {
-   menu.classList.add('nao-possui-azul');
-}
+console.log(itensMenu);
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.forEach((item) => {
+   item.classList.remove('ativo');
+});
 
-menu.className += ' vermelho'; 
+itensMenu[0].classList.add('ativo');
 
-console.log(menu.className);
+// Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll('img');
 
-//console.log(menu.classList.add)
+imgs.forEach((img) => {
+   const possuiAtributo= img.hasAttribute('alt');
+   console.log(img, possuiAtributo)
+})
 
-const animais = document.querySelector('.animais');
-console.log(animais.attributes['data-texto']);
+// Modifique o href do link externo no menu
+const link = document.querySelector('a[href^="http"');
+link.setAttribute('href', 'http://www.google.com/');
 
-const img = document.querySelector('img');
-
-console.log(img.getAttribute('src'));
+console.log(link);
